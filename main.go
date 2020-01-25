16b2073d576ce5cef32471c3f192394305fdce72
laurynasra <ragauskas.laurynas@gmail.com>
2020-01-26 01:24:44 +0200

    Working rooms management and announcments

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ type JoinRequest struct {
 	room string
 }
 
+type LeaveRequest struct {
+	ch   *chan string
+	room string
+}
+
 func main() {
 	broker := &Broker{
 		Rooms: map[string][]chan string{},
@@ -25,20 +30,28 @@ func main() {
 
 	router := httprouter.New()
 
-	joinChannel := make(chan *JoinRequest, 1)
-	leaveChannel := make(chan string, 1)
+	joinChannel := make(chan *JoinRequest)
+	leaveChannel := make(chan *LeaveRequest)
 
 	go func() {
 		for {
 			select {
 			case jr := <-joinChannel:
 				if _, ok := broker.Rooms[jr.room]; ok {
-					broker.Rooms[jr.room] = append(broker.Rooms["baras"], *jr.ch)
+					broker.Rooms[jr.room] = append(broker.Rooms[jr.room], *jr.ch)
 				} else {
 					roomChannels := make([]chan string, 0)
 					broker.Rooms[jr.room] = append(roomChannels, *jr.ch)
 				}
-			case <-leaveChannel:
+			case lr := <-leaveChannel:
+				roomChannels := broker.Rooms[lr.room]
+				fmt.Println("len of", lr.room, len(roomChannels))
+				for i := range roomChannels {
+					if roomChannels[i] == *lr.ch {
+						broker.Rooms[lr.room] = append(roomChannels[:i], roomChannels[i+1:]...)
+						close(*lr.ch)
+					}
+				}
 			}
 		}
 	}()
@@ -46,7 +59,8 @@ func main() {
 	// ch := make(chan string)
 
 	router.POST("/infocentras/:room", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		for _, ch := range broker.Rooms["baras"] {
+		room := ps.ByName("room")
+		for _, ch := range broker.Rooms[room] {
 			ch <- time.Now().Format(time.RFC850)
 		}
 		rw.WriteHeader(http.StatusCreated)
@@ -63,6 +77,7 @@ func main() {
 		room := ps.ByName("room")
 		ch := make(chan string)
 		joinChannel <- &JoinRequest{room: room, ch: &ch}
+
 		tick := time.Tick(time.Second * 30)
 
 		rw.Header().Set("Content-Type", "text/event-stream")
@@ -73,21 +88,25 @@ func main() {
 		fmt.Fprintf(rw, "data: %s\n\n", "labas")
 		flusher.Flush()
 
+		notify := rw.(http.CloseNotifier).CloseNotify()
+
 	Loop:
 		for {
 			select {
 			case msg := <-ch:
 				fmt.Fprintf(rw, "data: %s\n\n", msg)
 				flusher.Flush()
+			case <-notify:
+				println("The client closed the connection prematurely. Cleaning up.")
+				leaveChannel <- &LeaveRequest{room: room, ch: &ch}
+				break Loop
 			case <-tick:
-				close(ch)
-				// broker.Rooms[room] =
+				fmt.Fprintf(rw, "data: %s\n\n", "timeout")
+				flusher.Flush()
+				leaveChannel <- &LeaveRequest{room: room, ch: &ch}
 				break Loop
 			}
 		}
-
-		fmt.Fprintf(rw, "data: %s\n\n", "timeout")
-		flusher.Flush()
 	})
 
 	log.Fatal("HTTP Server error: ", http.ListenAndServe(":3000", router))
