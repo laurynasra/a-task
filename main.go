55db73850b2d725dcbde2958f87cc438e2f0066f
laurynasra <ragauskas.laurynas@gmail.com>
2020-01-26 14:44:44 +0200

    Functionally working

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -10,28 +11,44 @@ import (
 )
 
 type Broker struct {
-	Rooms map[string][]chan string
+	Rooms map[string][]chan Message
 }
 
-type JoinRequest struct {
-	ch   *chan string
+type ChannelRequest struct {
+	ch   *chan Message
 	room string
 }
 
-type LeaveRequest struct {
-	ch   *chan string
-	room string
+type Message struct {
+	id    int
+	event string
+	data  string
+}
+
+func sendMessage(rw http.ResponseWriter, msg Message) {
+	flusher, ok := rw.(http.Flusher)
+
+	if !ok {
+		http.Error(rw, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(rw, "id: %v\n", msg.id)
+	fmt.Fprintf(rw, "event: %s\n", msg.event)
+	fmt.Fprintf(rw, "data: %s\n\n", msg.data)
+
+	flusher.Flush()
 }
 
 func main() {
 	broker := &Broker{
-		Rooms: map[string][]chan string{},
+		Rooms: map[string][]chan Message{},
 	}
 
 	router := httprouter.New()
 
-	joinChannel := make(chan *JoinRequest)
-	leaveChannel := make(chan *LeaveRequest)
+	joinChannel := make(chan *ChannelRequest)
+	leaveChannel := make(chan *ChannelRequest)
 
 	go func() {
 		for {
@@ -40,12 +57,11 @@ func main() {
 				if _, ok := broker.Rooms[jr.room]; ok {
 					broker.Rooms[jr.room] = append(broker.Rooms[jr.room], *jr.ch)
 				} else {
-					roomChannels := make([]chan string, 0)
+					roomChannels := make([]chan Message, 0)
 					broker.Rooms[jr.room] = append(roomChannels, *jr.ch)
 				}
 			case lr := <-leaveChannel:
 				roomChannels := broker.Rooms[lr.room]
-				fmt.Println("len of", lr.room, len(roomChannels))
 				for i := range roomChannels {
 					if roomChannels[i] == *lr.ch {
 						broker.Rooms[lr.room] = append(roomChannels[:i], roomChannels[i+1:]...)
@@ -56,54 +72,48 @@ func main() {
 		}
 	}()
 
-	// ch := make(chan string)
+	messageId := 0
 
-	router.POST("/infocentras/:room", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	router.POST("/infocenter/:room", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		rw.Header().Set("Access-Control-Allow-Origin", "*")
+
+		b, _ := ioutil.ReadAll(r.Body)
 		room := ps.ByName("room")
 		for _, ch := range broker.Rooms[room] {
-			ch <- time.Now().Format(time.RFC850)
+			ch <- Message{id: messageId, event: "msg", data: string(b)}
 		}
-		rw.WriteHeader(http.StatusCreated)
+
+		messageId += 1
+		rw.WriteHeader(http.StatusNoContent)
 	})
 
-	router.GET("/infocentras/:room", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		flusher, ok := rw.(http.Flusher)
-
-		if !ok {
-			http.Error(rw, "Streaming unsupported!", http.StatusInternalServerError)
-			return
-		}
-
-		room := ps.ByName("room")
-		ch := make(chan string)
-		joinChannel <- &JoinRequest{room: room, ch: &ch}
-
-		tick := time.Tick(time.Second * 30)
-
+	router.GET("/infocenter/:room", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		rw.Header().Set("Content-Type", "text/event-stream")
 		rw.Header().Set("Cache-Control", "no-cache")
 		rw.Header().Set("Connection", "keep-alive")
 		rw.Header().Set("Access-Control-Allow-Origin", "*")
 
-		fmt.Fprintf(rw, "data: %s\n\n", "labas")
-		flusher.Flush()
-
+		room := ps.ByName("room")
+		ch := make(chan Message)
+		joinChannel <- &ChannelRequest{room: room, ch: &ch}
 		notify := rw.(http.CloseNotifier).CloseNotify()
+		tick := time.Tick(time.Second * 10)
+
+		defer func() {
+			leaveChannel <- &ChannelRequest{room: room, ch: &ch}
+		}()
 
 	Loop:
 		for {
 			select {
 			case msg := <-ch:
-				fmt.Fprintf(rw, "data: %s\n\n", msg)
-				flusher.Flush()
+				sendMessage(rw, msg)
 			case <-notify:
 				println("The client closed the connection prematurely. Cleaning up.")
-				leaveChannel <- &LeaveRequest{room: room, ch: &ch}
 				break Loop
 			case <-tick:
-				fmt.Fprintf(rw, "data: %s\n\n", "timeout")
-				flusher.Flush()
-				leaveChannel <- &LeaveRequest{room: room, ch: &ch}
+				sendMessage(rw, Message{event: "timeout", data: "30s"})
+				time.Sleep(1 * time.Second)
 				break Loop
 			}
 		}
